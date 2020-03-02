744efdba93d6b805628c6b4e55526690b08f604f
bacinsky <peter@bacinsky.sk>
2020-03-02 06:11:09 +0100

    Updated

--- Source/LoadingScreen/LoadingScreen.Build.cs
+++ Source/LoadingScreen/LoadingScreen.Build.cs
@@ -8,25 +8,20 @@ public class LoadingScreen : ModuleRules
     public LoadingScreen(ReadOnlyTargetRules Target)
         : base(Target)
     {
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
         PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
         PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
 
-        PublicDependencyModuleNames.AddRange(
-            new string[] {
-                    "Core",
-            });
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core"
+        });
 
-        PrivateDependencyModuleNames.AddRange(
-            new string[] {
-                    "CoreUObject",
-                    "MoviePlayer",
-                    "Slate",
-                    "SlateCore",
-                    "InputCore",
-                    "Engine"
-            });
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            "CoreUObject",
+            "MoviePlayer",
+            "Slate",
+            "SlateCore",
+            "InputCore",
+            "Engine"
+        });
     }
 }
-
