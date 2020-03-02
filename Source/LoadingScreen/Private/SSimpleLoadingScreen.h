fa85b4103d01fe5c9e5e37163db056b486954cc4
bacinsky <peter@bacinsky.sk>
2020-03-02 06:46:01 +0100

    Updated

--- Source/LoadingScreen/Private/SSimpleLoadingScreen.h
+++ Source/LoadingScreen/Private/SSimpleLoadingScreen.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#include "SCompoundWidget.h"
-#include "CoreStyle.h"
+#include "Widgets/SCompoundWidget.h"
+#include "Styling/CoreStyle.h"
 #include "LoadingScreenSettings.h"
 
 class FDeferredCleanupSlateBrush;
